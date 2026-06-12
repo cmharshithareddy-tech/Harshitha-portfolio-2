6f0d5c4a3f223691b117290a00ad4c78cf686006
cmharshithareddy-tech <cmharshithareddy@gmail.com>
2026-06-12 20:55:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const btn =
+document.getElementById("themeBtn");
+
+btn.addEventListener("click", () => {
+
+document.body.classList.toggle("dark");
+
+if(document.body.classList.contains("dark")){
+
+btn.innerHTML =
+"☀️ Light Mode";
+
+}
+else{
+
+btn.innerHTML =
+"🌙 Dark Mode";
+
+}
+
+});
